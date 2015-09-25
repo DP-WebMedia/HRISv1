aece884269478d893a29f60539136195bf97b0d0
Mark Daniel G. Bandilla <markbandilla.dpwm@yahoo.com>
2015-09-26 01:36:00 +0800

    create module 2 on test php union after some time

--- union.php
+++ union.php
@@ -6,6 +6,10 @@ function module1() {
     echo "waiting for union";
 }
 
+function module2() {
+    echo "made after some time";
+}
+
 //Module Two goes here ----------------------------------
     
 ?>
