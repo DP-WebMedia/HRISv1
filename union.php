eed4fd18462fae89022841030ebcc3e256053837
Mark Daniel G. Bandilla <markbandilla.dpwm@yahoo.com>
2015-09-26 01:32:17 +0800

    module one for php union test has been created

--- union.php
+++ union.php
@@ -2,6 +2,10 @@
 
 //Module One goes here ----------------------------------
 
+function module1() {
+    echo "waiting for union";
+}
+
 //Module Two goes here ----------------------------------
     
 ?>
